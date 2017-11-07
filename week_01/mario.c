fb8164570c379f77bec2a820e14a528f78379cf3
Lynn Long Jr <lynnlong000@gmail.com>
2017-11-07 05:17:12 +0000

    refactor to have a function or method what ever you call it in C

--- week_01/mario.c
+++ week_01/mario.c
@@ -1,55 +1,43 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int mario(int height);
+
 int main (void){
     int height = get_int("Get height of mario's pyramid: ");
-    int hash = 1;
-    int space = 0;
 
     if ( height < 24 && height > 0){
-        space = height - hash;
-        for(int i = 0; i < height; i++){
-            for (int j = 0; j < space; j++){
-                printf(" ");
-            }
-
-            for (int j = 0; j < hash; j++){
-                printf("#");
-            }
-
-            printf("  ");
-
-            for(int j = 0; j < hash; j++){
-                printf("#");
-            }
-
-            printf("\n");
-            hash++;
-            space--;
-        }
+        mario(height);
     } else if (height < 0){
-        printf("wrong");
+        printf("wrong\n");
     } else if (height > 23){
         height = height / 10;
-        space = height - hash;
-        for(int i = 0; i < height; i++){
-            for (int j = 0; j < space; j++){
-                printf(" ");
-            }
-
-            for (int j = 0; j < hash; j++){
-                printf("#");
-            }
-
-            printf("  ");
-
-            for(int j = 0; j < hash; j++){
-                printf("#");
-            }
-
-            printf("\n");
-            hash++;
-            space--;
-        }
+        mario(height);
     }
+}
+
+int mario(int height){
+    int hash = 1;
+    int space = 0;
+    space = height - hash;
+    for(int i = 0; i < height; i++){
+        for (int j = 0; j < space; j++){
+            printf(" ");
+        }
+
+        for (int j = 0; j < hash; j++){
+            printf("#");
+        }
+
+        printf("  ");
+
+        for(int j = 0; j < hash; j++){
+            printf("#");
+        }
+
+        printf("\n");
+        hash++;
+        space--;
+    }
+    return 0;
 }
